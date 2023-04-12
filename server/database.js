f761b48bbb0d92f7f0549fde5425a3817b002f7f
sueys <sueys@live.fr>
2023-04-12 09:55:47 +0200

    départ de création de db

---
+++ server/database.js
@@ -0,0 +1,13 @@
+const mysql =require ('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.DB_PORT,
+
+})
